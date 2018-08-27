bb49f5e2c0cb3f5b260b91515fd4c8520bc8f5ce
Felix Kurniawan <fkurniaw@uwaterloo.ca>
2018-08-26 18:24:41 -0600

    add cumulative season average tracker

--- src/components/Player/PlayerGameLog/PlayerGameLog.js
+++ src/components/Player/PlayerGameLog/PlayerGameLog.js
@@ -25,11 +25,74 @@ class PlayerGameLog extends React.Component {
       console.info(err);
     });
   }
+  renderCumulativeAverages() {
+    const rowsToSkip = [7, 10, 13, 21];
+    let headerCells = headers.map(stat => {
+      return (
+        <Table.HeaderCell key={stat} className='player-game-log-cell-headers'>
+          {stat}
+        </Table.HeaderCell>
+      );
+    });
+    let averages = new Array(23).fill(0, 0, 23);
+
+    let rows = this.props.playerGameLog.map((game, i) => {
+      averages[0] = i + 1;
+      averages[1] = game.game_date;
+      averages[2] = game.matchup;
+      averages[3] = game.wl;
+      averages[4] += game.min;
+      averages[5] += game.fgm;
+      averages[6] += game.fga;
+      averages[7] = averages[6] === 0 ? 0 : Math.round((averages[5] / averages[6]) * 1000) / 1000;
+      averages[8] += game.fg3m;
+      averages[9] += game.fg3a;
+      averages[10] = averages[9] === 0 ? 0 : Math.round((averages[8] / averages[9]) * 1000) / 1000;
+      averages[11] += game.ftm;
+      averages[12] += game.fta;
+      averages[13] = averages[12] === 0 ? 0 : Math.round((averages[11] / averages[12]) * 1000) / 1000;
+      averages[14] += game.oreb;
+      averages[15] += game.dreb;
+      averages[16] += game.ast;
+      averages[17] += game.stl;
+      averages[18] += game.blk;
+      averages[19] += game.pf;
+      averages[20] += game.tov;
+      averages[21] += game.plus_minus;
+      averages[22] += game.pts;
+      let cells = averages.map((stat, j) => (
+        <Table.Cell className='player-game-log-stat' key={j}>
+          {j < 4 || rowsToSkip.includes(j) ? stat : Math.round((stat / (i + 1)) * 10) / 10}
+        </Table.Cell>
+      ));
+      return (
+        <Table.Row key={i}>
+          {cells}
+        </Table.Row>
+      );
+    });
+
+    return (
+      <div className='player-game-log-table-wrapper'>
+        <h3 className='player-game-log-header'>Cumulative Season Average Game Log</h3>
+        <Table className='player-game-log-table'>
+          <Table.Header>
+            <Table.Row>
+              {headerCells}
+            </Table.Row>
+          </Table.Header>
+          <Table.Body>
+            {rows}
+          </Table.Body>
+        </Table>
+      </div>
+    );
+  }
   renderGameLog() {
     let rows = [];
     let headerCells = headers.map(stat => {
       return (
-        <Table.HeaderCell key={stat} className='player-game-log-headers'>
+        <Table.HeaderCell key={stat} className='player-game-log-cell-headers'>
           {stat}
         </Table.HeaderCell>
       );
@@ -65,16 +128,19 @@ class PlayerGameLog extends React.Component {
       );
     });
     return (
-      <Table className='player-game-log-table' collapsing stackable>
-        <Table.Header>
-          <Table.Row>
-            {headerCells}
-          </Table.Row>
-        </Table.Header>
-        <Table.Body>
-          {rows}
-        </Table.Body>
-      </Table>
+      <div className='player-game-log-table-wrapper'>
+        <h3 className='player-game-log-header'>Regular Season Game Log</h3>
+        <Table className='player-game-log-table' collapsing stackable>
+          <Table.Header>
+            <Table.Row>
+              {headerCells}
+            </Table.Row>
+          </Table.Header>
+          <Table.Body>
+            {rows}
+          </Table.Body>
+        </Table>
+      </div>
     );
   }
   render() {
@@ -82,6 +148,7 @@ class PlayerGameLog extends React.Component {
       <div className='player-game-log'>
         <h1>{this.props.playerName}</h1>
         {this.props.playerGameLog.length > 0 && this.renderGameLog()}
+        {this.props.playerGameLog.length > 0 && this.renderCumulativeAverages()}
       </div>
     );
   }
