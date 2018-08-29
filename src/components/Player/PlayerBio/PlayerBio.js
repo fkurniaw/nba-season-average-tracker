f8613713c7d98180b7e897e8d8caced5c49ec9ce
Felix Kurniawan <fkurniaw@uwaterloo.ca>
2018-08-28 22:58:52 -0600

    add initial PlayerBio actions and sample headers

--- src/components/Player/PlayerBio/PlayerBio.js
+++ src/components/Player/PlayerBio/PlayerBio.js
@@ -5,15 +5,25 @@ import { connect } from 'react-redux';
 import Sources from '../../../util/sources';
 import * as actions from '../../../redux/actionCreators/playersActions';
 
+import './playerBio.css';
+
 class PlayerBio extends Component {
   componentDidMount() {
     Sources.getPlayerBio(this.props.id).then(res => {
-      actions.setPlayerBio(res.data);
+      this.props.setPlayerName(res.data.CommonPlayerInfo[0].display_first_last);
+      this.props.setPlayerBio({
+        headlineStats: res.data.PlayerHeadlineStats[0],
+        playerInfo: res.data.CommonPlayerInfo[0]
+      });
     }).catch(err => console.info(err));
   }
   render() {
     return (
       <div className='player-bio'>
+        <h5 className='player-bio-left-headers'>Birth Date:</h5>
+        <span>{this.props.playerBio.playerInfo ? this.props.playerBio.playerInfo.birthdate.slice(0, 10) : ''}</span>
+        <h5 className='player-bio-left-headers'>School:</h5>
+        <span>{this.props.playerBio.playerInfo ? this.props.playerBio.playerInfo.school : ''}</span>
       </div>
     );
   }
@@ -31,7 +41,10 @@ const actionCreators = {
 };
 
 PlayerBio.propTypes = {
-  id: PropTypes.string
+  id: PropTypes.string,
+  playerBio: PropTypes.object,
+  setPlayerBio: PropTypes.func,
+  setPlayerName: PropTypes.func
 };
 
 export default connect(mapStateToProps, actionCreators)(PlayerBio);
--- src/redux/initialState.js
+++ src/redux/initialState.js
@@ -1,6 +1,7 @@
 export const initialPlayersState = {
   currentPlayer: {},
   players: {},
+  playerBio: {},
   playerCumulativeAverageGameLog: [],
   playerCumulativeTotalGameLog: [],
   playerGameLog: [],
