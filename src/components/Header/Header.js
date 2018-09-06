b1541ee14aa27b7a2b52dca1e673ae1bd5dcfdc9
Felix Kurniawan <fkurniaw@uwaterloo.ca>
2018-09-05 20:28:15 -0400

    add div spacer for overflow

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -17,12 +17,15 @@ class Header extends Component {
   }
   render() {
     return (
-      <header className="App-header">
-        <AppMenu activeItem={this.state.activeItem} app={this} baseUrl={this.props.baseUrl} history={this.props.history} links={this.props.links} menuItems={this.props.headerNames}/>
-        <div className='App-title-wrapper'>
-          <h1 className="App-title">{this.props.headerNames[this.state.activeItem]}</h1>
-        </div>
-      </header>
+      <div>
+        <header className="App-header">
+          <AppMenu activeItem={this.state.activeItem} app={this} baseUrl={this.props.baseUrl} history={this.props.history} links={this.props.links} menuItems={this.props.headerNames}/>
+          <div className='App-title-wrapper'>
+            <h1 className="App-title">{this.props.headerNames[this.state.activeItem]}</h1>
+          </div>
+        </header>
+        <div className='spacer' />
+      </div>
     );
   }
 };
